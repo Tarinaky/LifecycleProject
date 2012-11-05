73833abc3990e6a7689105a95e688f7a280faf78
Tarinaky <tarinaky@gmail.com>
2012-11-05 16:46:45 +0000

    Added javadoc.

--- src/uk/ac/aber/dcs/cs221/monstermash/data/Monster.java
+++ src/uk/ac/aber/dcs/cs221/monstermash/data/Monster.java
@@ -30,6 +30,12 @@ public class Monster {
 	
 	private volatile int injuries;
 	
+	/**
+	 * Public constructor.
+	 * 
+	 * Assigns the next available primary key and initialises the date of
+	 * birth field to this instant.
+	 */
 	public Monster() {
 		synchronized (getClass() ) {
 			primaryKey = nextPrimaryKey++;	
@@ -40,30 +46,77 @@ public class Monster {
 		injuries = 0;
 	}
 	
+	/**
+	 * 
+	 * @return This monster's primary key.
+	 */
 	public long getUID() { return primaryKey; }
 	
+	/**
+	 * 
+	 * @param owner The monster's owner.
+	 * @return This monster object.
+	 */
 	public synchronized Monster setOwner(UserAccount owner) { this.owner = owner; return this; }
+	/**
+	 * 
+	 * @return The monster's owner.
+	 */
 	public UserAccount getOwner() { return owner; }
 	
+	/**
+	 * Customise a monster with a name.
+	 * @param name The monster's new name.
+	 * @return The monster object.
+	 */
 	public synchronized Monster setName(String name) { this.name = name; return this; }
+	/**
+	 * 
+	 * @return The monster's name.
+	 */
 	public String getName() { return name; }
 	
+	/**
+	 * @see Gender
+	 * @param gender The monster's gender.
+	 * @return This monster object.
+	 */
 	public synchronized Monster setGender(Gender gender) { this.gender = gender; return this; }
+	/**
+	 * 
+ 	 * @return True iff this monster is male.
+	 */
 	public boolean isMale() { return (gender == Gender.MALE); }
-	
+	/**
+	 * 
+	 * @return The monster's age in seconds.
+	 */
 	public long getAge() { return dateOfBirth.getTime(); }
+	/**
+	 * 
+	 * @return The monster's health, less than 1. If less than 0 this indicates the monster should be reaped.
+	 */
 	public double getHealth() {
 		return 2 - Math.exp(ageRate * getAge() ); //$2-e^{\lambda t}$
 	}
-	
+	/**
+	 * 
+	 * @return The monster's strength score.
+	 */
 	public int getStrength() {
 		return (int) (strengthCoefficient*(Math.exp(ageRate * getAge() ) - 1 ) * getHealth());
 	}
-	
+	/**
+	 * 
+	 * @return The monster's chance at evading an incoming blow.
+	 */
 	public int getEvade() {
 		return (int) (evadeCoefficient*(Math.exp(ageRate * getAge() ) - 1) * getHealth() );
 	}
-	
+	/**
+	 * 
+	 * @return The monster's toughness score.
+	 */
 	public int getToughness() {
 		return (int) (toughnessCoefficient*(Math.exp(ageRate * getAge() ) - 1) * getHealth() );
 	}
