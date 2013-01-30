8359d0d16ccfcae390f7947182336f332b553d43
Tarinaky <tarinaky@gmail.com>
2013-01-30 14:45:37 +0000

    Added ServerTable to DataSingleton.

--- src/uk/ac/aber/dcs/cs221/monstermash/data/DataSingleton.java
+++ src/uk/ac/aber/dcs/cs221/monstermash/data/DataSingleton.java
@@ -9,15 +9,38 @@ import java.util.TimerTask;
 
 import org.json.JSONException;
 
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
+
+import uk.ac.aber.dcs.cs221.monstermash.data.remote.ServerTable;
+
 public class DataSingleton {
 	
 	static volatile TableOfAccounts instance = null;
+	static volatile ServerTable remote = null;
+	
 	final static String dataPath = "monstermash_data_persistance.json";
 	final static int serialisationPeriod = 60000;//One minute in milliseconds.
+	
+	
 	
 	public static synchronized TableOfAccounts get() {
 		if (instance == null) {
 			instance = new TableOfAccounts();//Initialise
+			
+			//Load remote server directory.
+			try {
+				remote = new ServerTable().load();
+			} catch (UniformInterfaceException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (ClientHandlerException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (JSONException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			
 			//Try to load from file.
 			try {
