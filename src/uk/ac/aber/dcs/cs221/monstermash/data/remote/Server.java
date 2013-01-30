60bccf7abb45e58c1254b3ea8252eef3a0017777
Tarinaky <tarinaky@gmail.com>
2013-01-30 13:56:53 +0000

    Added basic RemoteUser functionality.

--- src/uk/ac/aber/dcs/cs221/monstermash/data/remote/Server.java
+++ src/uk/ac/aber/dcs/cs221/monstermash/data/remote/Server.java
@@ -3,16 +3,25 @@ package uk.ac.aber.dcs.cs221.monstermash.data.remote;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+
 public class Server {
 
 	private volatile String httpAddress="NoName";
 	private volatile String serverName="NoAddress";
 	
+	private volatile Client client;
+	private final static String userLookup = "/users?userID=";
+	
 	public Server() {
+		client = new Client();
 	}
 	
 	
-	public Server readJSON(JSONObject json) throws JSONException {
+	public synchronized Server readJSON(JSONObject json) throws JSONException {
 		serverName = "Group"+json.getInt("serverNumber");
 		httpAddress = json.getString("httpRoot");
 		return this;		
@@ -27,9 +36,11 @@ public class Server {
 	}
 	
 	
-	public synchronized RemoteUser lookup(long uid) {
-		return null;
-		// TODO: implement this
+	public synchronized RemoteUser lookup(String uid) throws UniformInterfaceException, ClientHandlerException, JSONException {
+		WebResource r = client.resource(getAddress() + userLookup +uid);
+		JSONObject json = new JSONObject(r.get(String.class) );
+		RemoteUser user = new RemoteUser().readJSON(json);
+		return user;
 	}
 
 }
