9a93d6b8dacb753afb5f8fc0499381070b7a6d22
Ivan Cholakov <ivc@aber.ac.uk>
2013-02-01 09:58:20 +0000

    Updated test data generator to match expectations from bernie. Note: due to short monster lifespans the generator will have to be rerun.

--- src/aber/dcs/cs221/group5/util/GenerateTestData.java
+++ src/aber/dcs/cs221/group5/util/GenerateTestData.java
@@ -6,7 +6,9 @@ import java.io.IOException;
 
 import org.json.JSONException;
 
+import aber.dcs.cs221.group5.data.Monster;
 import aber.dcs.cs221.group5.data.TableOfAccounts;
+import aber.dcs.cs221.group5.data.UserAccount;
 
 
 public class GenerateTestData {
@@ -15,8 +17,15 @@ public class GenerateTestData {
 	
 		TableOfAccounts data = new TableOfAccounts();
 		
-		data.addUser("hello1.gmail.com").setPassword("test1");
+		UserAccount user = data.addUser("hello1@gmail.com").setPassword("test1");
+		Monster.generateRandom().setOwner(user);
+		Monster.generateRandom().setOwner(user);
+		Monster.generateRandom().setOwner(user);
 		data.addUser("james23@hotmail.com").setPassword("test2");
+		Monster.generateRandom().setOwner(data.lookup("james23@hotmail.com"));
+		Monster.generateRandom().setOwner(data.lookup("james23@hotmail.com"));
+		Monster.generateRandom().setOwner(data.lookup("james23@hotmail.com"));
+		
 		data.addUser("ck2@aol.co.uk").setPassword("test3");
 		data.addUser("byebye8@outlook.co.uk").setPassword("test4");
 		data.addUser("wonkado27@live.co.uk").setPassword("test5");
